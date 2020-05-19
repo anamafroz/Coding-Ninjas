be060162a640919cf534112f1a117d7a1dd9926f
Anam Afroz <35655608+anamafroz@users.noreply.github.com>
2020-05-20 01:37:47 +0530

    Create Star Pattern.java

---
+++ Patterns 2/Star Pattern.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		Scanner scan =new Scanner(System.in);
+		int n = scan.nextInt();
+        int i=1;
+        while(i<=n){
+            int j=1;
+            while(j<=n-i){
+                System.out.print(" ");
+                j++;
+            }
+            j=1;
+            int k=1;
+            while(j<=i){
+                System.out.print("*");
+                j++;
+            }
+            j=1;
+            while(j<=i-1){
+                System.out.print("*");
+                j++;
+            }
+             System.out.println();
+            i++;
+        }
+		
+	}
+
+}
