c74d20f82687ab2dff0d97fe77f223c4d45eccda
Anam Afroz <35655608+anamafroz@users.noreply.github.com>
2020-05-20 01:03:57 +0530

    Create Triangle Number Pattern.java

---
+++ Patterns 1/Triangle Number Pattern.java
@@ -0,0 +1,34 @@
+///**
+
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+22
+333
+4444
+
+**////
+
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		Scanner scan =new Scanner(System.in);
+		int n = scan.nextInt();
+		int i=1;
+		while(i<=n) {
+			int j=1;
+			while(j<=i) {
+				System.out.print(i);
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+		
+	}
+
+}
