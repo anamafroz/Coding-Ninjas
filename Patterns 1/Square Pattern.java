0fa5e73e604f8a2ab6304b0a29988378f25a142c
Anam Afroz <35655608+anamafroz@users.noreply.github.com>
2020-05-20 00:59:49 +0530

    Create Square Pattern.java

---
+++ Patterns 1/Square Pattern.java
@@ -0,0 +1,33 @@
+//***
+
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+4444
+4444
+4444
+4444
+
+******///
+
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		Scanner scan =new Scanner(System.in);
+		int n = scan.nextInt();
+		int i=1;
+		while(i<=n) {
+			int j=1;
+			while(j<=n) {
+				System.out.print(n);
+				j++;
+			}
+			System.out.println();
+			i=i+1;
+		}
+		
+	}
+
+}
